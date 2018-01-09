ada6d4080f4c0704e6571803390ebe410ff3fdd4
Vineet Joshi <vineet0713@gmail.com>
2018-01-08 19:58:46 -0800

    Add functionality to application (UIView changes color when Sliders change their values).

--- Color Palette/ViewController.swift
+++ Color Palette/ViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func colorChanged() {
+        let r = CGFloat(redSlider.value)
+        let g = CGFloat(greenSlider.value)
+        let b = CGFloat(blueSlider.value)
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1.0)
     }
-
-
 }
 
